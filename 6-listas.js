3bb0516b25db129e64bd65e27bc8a771889103a3
Paulo Moutinho <paulo_vicali@icloud.com>
2021-09-25 14:51:31 -0300

    aprendendo mais sobre listas e new Array

--- 6-listas.js
+++ 6-listas.js
@@ -4,16 +4,31 @@ console.log(`Trabalhando com variáveis`)
 // const rioDeJaneiro = `Rio de Janeiro`
 // const salvador = `Salvador`
 
+
+//Para facilitar e agiliar o processo, existe uma estrutura de dados chamada array, 
+//em que podemos armazenar diversos dados. A maneira de declarar um array é um pouco diferente de textos e números:
+// utilizamos uma palavra chave new e o tipo da estrutura Array, e assim podemos criar uma nova lista com nossos destinos:
 const listaDeDestinos = new Array(
     `São Paulo`,
     `Rio de Janeiro`,
     `Salvador`,
     `Curitiba`,
     `Porto Alegre`
-) 
+)
 
 listaDeDestinos.push(`Cutia`) //adicionando itens na lista com a função (.push) na variável "listaDeDestinosj"
 
 console.log('Destinos Possíveis:')
-console.log( listaDeDestinos )
+console.log(listaDeDestinos)
 
+//Sala de cinema
+const listaDeFilmes = new Array(
+    'Poderoso Chefão',
+    'Herry Potter',
+    'Lagoa Azul',
+    'O Erxocista',
+    'Velozos e furiosos'
+)
+
+console.log(`Lista de filmes hoje á noite`)
+console.log(listaDeFilmes)
